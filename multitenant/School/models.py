7c995e2f845e535b16b2b4b3bda1a31fff6a9709
nealwaga <waganeal@gmail.com>
2023-03-02 16:34:37 +0300

    added model

--- multitenant/School/models.py
+++ multitenant/School/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    registration_number = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.registration_number
