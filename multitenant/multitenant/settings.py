865ef225381ecf810b931caf0cb2d12f2d232b20
nealwaga <waganeal@gmail.com>
2023-03-02 16:28:33 +0300

    connected django to mongodb

--- multitenant/multitenant/settings.py
+++ multitenant/multitenant/settings.py
@@ -77,10 +77,7 @@ WSGI_APPLICATION = 'multitenant.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {'ENGINE':'djongo', 'NAME':'default'},
 }
 
 
