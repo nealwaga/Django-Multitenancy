f5790c34bf28ece73f7cf092def76f83cae9aad0
nealwaga <waganeal@gmail.com>
2023-03-02 16:39:37 +0300

    created view

--- multitenant/School/views.py
+++ multitenant/School/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Students
 
 # Create your views here.
+def get_Students(request):
+    students = Students.objects.all()
+    context = {
+        'students': students
+    }
+    return render(request, 'index.html', context)
