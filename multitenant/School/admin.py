7fc14dd908b46977614c60516a981cf9fa1217f8
nealwaga <waganeal@gmail.com>
2023-03-02 16:37:22 +0300

    registered model

--- multitenant/School/admin.py
+++ multitenant/School/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Students
 
 # Register your models here.
+class adminStudent(admin.Model):
+    list_display = ["registration_number", "first_name", "last_name"]
+
+admin.site.register(adminStudent, Students)
